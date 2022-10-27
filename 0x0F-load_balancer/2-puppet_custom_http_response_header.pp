72b76e90daedb4b28627bb0c839bd2b8a8a02531
Kitesa <kitesamerga@gmail.com>
2022-10-27 01:03:53 -0700

    Added pppet command

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,5 +1,19 @@
-# Using exec to kill a process named killmenow
-exec {'kill-killmenow':
-  command => 'pkill killmenow',
-  path    => '/usr/bin';
+#puppet advance
+exec { 'update':
+  command  => 'sudo apt-get update',
+  provider => shell,
+}
+-> package {'nginx':
+  ensure => present,
+}
+-> file_line { 'header line':
+  ensure => present,
+  path   => '/etc/nginx/sites-available/default',
+  line   => "	location / {
+  add_header X-Served-By ${hostname};",
+  match  => '^\tlocation / {',
+}
+-> exec { 'restart service':
+  command  => 'sudo service nginx restart',
+  provider => shell,
 }
